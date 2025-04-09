79a792fff620c2f8f7643a968a11782634e93ce3
Leon Munene <leonmunene@gmail.com>
2025-04-09 10:10:14 +0300

    style

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,21 +3,26 @@ const Home = () => {
       <div className="relative items-center justify-center w-full overflow-x-hidden lg:pt-40 lg:pb-40 xl:pt-40 xl:pb-64">
         <div className="container flex flex-col items-center justify-between h-full max-w-6xl px-8 mx-auto -mt-32 lg:flex-row xl:px-0">
           <div className="z-30 flex flex-col items-center w-full max-w-xl pt-48 text-center lg:items-start lg:w-1/2 lg:pt-20 xl:pt-40 lg:text-left">
-            <h1 className="relative mb-4 text-3xl font-black leading-tight text-gray-900 sm:text-6xl xl:mb-8">
+            <h1 className="relative z-20 mb-4 text-3xl font-black leading-tight text-gray-900 sm:text-6xl xl:mb-8 ">
               Affordable, Fast Software Solutions
             </h1>
             <p className="pr-0 mb-8 text-base text-gray-600 sm:text-lg xl:text-xl lg:pr-20">
               We build websites and apps quickly and affordably without compromising on quality.
             </p>
+            <div className="flex space-x-4">
             <a href="/pricing" className="relative self-start inline-block w-auto px-8 py-4 mx-auto mt-0 text-base font-bold text-white bg-indigo-600 border-t border-gray-200 rounded-md shadow-xl sm:mt-1 fold-bold lg:mx-0">
               View Pricing
             </a>
+            <a href="/projects" className="relative self-start inline-block w-auto px-8 py-4 mx-auto mt-0 text-base font-bold text-white bg-indigo-600 border-t border-gray-200 rounded-md shadow-xl sm:mt-1 fold-bold lg:mx-0">
+              What we have done
+            </a>
+            </div>
           </div>
           
           <div className="relative z-50 flex flex-col items-end justify-center w-full h-full lg:w-1/2 ms:pl-10">
             <div className="container relative left-0 w-full max-w-4xl lg:absolute xl:max-w-6xl lg:w-screen">
               <img src='/web.jpg'
-                className="w-full h-auto mt-20 mb-20 ml-0 lg:mt-24 xl:mt-40 lg:mb-0 lg:h-full lg:-ml-12" 
+                className="z-19 w-full h-auto mt-20 mb-20 ml-0 lg:mt-24 xl:mt-40 lg:mb-0 lg:h-full lg:-ml-12" 
                 alt="Denex Software" />
             </div>
           </div>
